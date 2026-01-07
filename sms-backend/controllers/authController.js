dfb853b5a99f4f5f23b579329970b2ad11d5d16f
Manukumar664 <manu@sestinfotech.com>
2026-01-07 12:59:08 +0530

    add

--- sms-backend/controllers/authController.js
+++ sms-backend/controllers/authController.js
@@ -58,6 +58,7 @@ const login = async (req, res) => {
   res.cookie("token",token,{
     httpOnly:true,
     sameSite:"none",
+    secure:true,
     maxAge:24*60*60*1000
   })
     // ✅ Send success response
