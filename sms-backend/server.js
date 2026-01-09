b1d47d1c68158effd7a0575c203154e3ea9cc7eb
Manukumar664 <manu@sestinfotech.com>
2026-01-09 13:08:51 +0530

    hello

--- sms-backend/server.js
+++ sms-backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 // ==========================
    app.use(cors({ origin: "https://school-timetable-system-ten.vercel.app", credentials: true }));
 
-  //app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+ // app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
